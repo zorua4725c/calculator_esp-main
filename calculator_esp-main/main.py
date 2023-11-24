f23fb39752d4d5499908667dbe1333c5eda16d7c
zorua4725c <zorua4725c@gmail.com>
2023-11-24 20:25:01 -0300

    calculadora ecologica

--- calculator_esp-main/main.py
+++ calculator_esp-main/main.py
@@ -1,5 +1,5 @@
 # Importar
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -15,8 +15,7 @@ def result_calculate(size, lights, device):
 @app.route('/')
 def index():
     return render_template('index.html')
-
-# La segunda página
+# Segunda página
 @app.route('/<size>')
 def lights(size):
     return render_template(
@@ -28,7 +27,7 @@ def lights(size):
 @app.route('/<size>/<lights>')
 def electronics(size, lights):
     return render_template(
-                            'electronics.html',
+                            'electronics.html',                           
                             size = size, 
                             lights = lights                           
                            )
@@ -42,4 +41,24 @@ def end(size, lights, device):
                                                     int(device)
                                                     )
                         )
+# El formulario
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+#Resultados del formulario
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    # Declarar variables para la recogida de datos
+    name = request.form['name']
+    email = request.form['email']
+    address = request.form['address']
+    date = request.form['date']
+
+    # Puedes guardar tus datos o enviarlos por correo electrónico
+    return render_template('form_result.html', 
+                           # Coloque aquí las variables
+                           name=name, email=email, address=address, date=date
+                           )
+
 app.run(debug=True)
